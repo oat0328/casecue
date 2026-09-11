d274b1086c7ded71f847662367b94f58b7ad3826
Edy Dollisme <macbookgenius007@gmail.com>
2026-09-11 04:31:43 +0000

    External agent changes

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -19,7 +19,7 @@ const ORG_ENTITIES = [
   'Student', 'Goal', 'ProgressData', 'SessionLog', 'GradebookAssignment',
   'Document', 'Meeting', 'Lesson', 'SubPlan', 'ScheduleEntry', 'SavedReport',
   'IEPReview', 'AIConversation', 'Task', 'IepWorkspace', 'SessionRecord', 'MeetingCheatSheet',
-  'ParentResource', 'TransitionPlan', 'Modification', 'AccommodationLog', 'ResourceItem', 'EvaluationRecord', 'RetentionPolicy', 'WorkEvidence', 'InputRecord', 'FamilyRequest',
+  'ParentResource', 'TransitionPlan', 'Modification', 'AccommodationLog', 'ResourceItem', 'EvaluationRecord', 'RetentionPolicy', 'WorkEvidence', 'InputRecord', 'FamilyRequest', 'InterventionPhase',
 ];
 
 let cachedOrgId;
